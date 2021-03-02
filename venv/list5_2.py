79607fa74c96c1b4eb636de1573cbfb3cfa40212
buffik1989 <buffik1989@gmail.com>
2021-03-02 16:22:49 +0300

    02.03.2021

---
+++ venv/list5_2.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+'''
+Задание 5.2
+
+Запросить у пользователя ввод IP-сети в формате: 10.1.1.0/24
+
+Затем вывести информацию о сети и маске в таком формате:
+
+Network:
+10        1         1         0
+00001010  00000001  00000001  00000000
+
+Mask:
+/24
+255       255       255       0
+11111111  11111111  11111111  00000000
+
+Проверить работу скрипта на разных комбинациях сеть/маска.
+
+Ограничение: Все задания надо выполнять используя только пройденные темы.
+'''
+
+interface = input('Enter interface type and number: ')
+vlan = input('Enter VLAN number: ')
+
+access_template = ['switchport mode access',
+                   'switchport access vlan {}',
+                   'switchport nonegotiate',
+                   'spanning-tree portfast',
+                   'spanning-tree bpduguard enable']
+
+print('\n' + '-' * 30)
+print('interface {}'.format(interface))
+print('\n'.join(access_template).format(vlan))
+
+ip = input()
+print('Ввведите ip: {}.{}.{}.{}/{}'.format(ip, ip, ip, ip, ip))
